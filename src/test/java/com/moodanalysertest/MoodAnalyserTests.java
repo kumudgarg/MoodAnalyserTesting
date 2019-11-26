ba009211602023cd07c27bb41c2f73b8ebd70f9f
kumudgarg <kumud50garg@gmail.com>
2019-11-26 11:32:03 +0530

    Refactor : gave message in parameter constructor instead of method

--- src/test/java/com/moodanalysertest/MoodAnalyserTests.java
+++ src/test/java/com/moodanalysertest/MoodAnalyserTests.java
@@ -8,15 +8,15 @@ public class MoodAnalyserTests {
 
     @Test
     public void givenMesage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("“I am in Sad Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",mood);
     }
 
     @Test
     public void givenMesage_WhenHappy_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("“I am in Happy Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("Happy",mood);
     }
 
